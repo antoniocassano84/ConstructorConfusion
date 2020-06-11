824adbf9ae01fb7ef10ebc560bf910461c2e6832
antoniocassano84 <antonio.cassano984@gmail.com>
2020-06-11 16:17:21 +0100

    remove config tags

--- src/main/java/com/apress/prospring5/ch3/xml/ConfigurableMessageProvider.java
+++ src/main/java/com/apress/prospring5/ch3/xml/ConfigurableMessageProvider.java
@@ -1,13 +1,8 @@
 package com.apress.prospring5.ch3.xml;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service("provider")
 public class ConfigurableMessageProvider implements MessageProvider {
   private String message;
 
-  @Autowired
   public ConfigurableMessageProvider(String message) {
     this.message = message;
   }
